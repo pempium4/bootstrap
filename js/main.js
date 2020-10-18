7f14be52765d82663a264f01d89efa23d39bbc9d
Pempium4 <front.shipper@gmail.com>
2020-10-18 16:00:09 +0300

    Media (slick slider)

--- js/main.js
+++ js/main.js
@@ -22,5 +22,32 @@ $(function (){
             scrollTop: $(target).offset().top - navHeight
         }, 500);
     });
-
+    $('#slick img').click(function (){
+        let newAttr = $(this).attr('src');
+        let currImg = $('#show-slide');
+        if(newAttr !== currImg.attr('src')){
+            currImg.hide(1).attr('src', newAttr).fadeIn(500);
+        }
+    });
+    $('#slick').slick({
+        arrows: false,
+        autoplay: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    autoplay: true,
+                    autoplaySpeed: 5000,
+                    dots: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    vertical: true,
+                    verticalSwiping: true
+                }
+            }
+        ]
+    });
 });
